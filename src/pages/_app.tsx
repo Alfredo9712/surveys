db2773d0be5eae19167ffa2fd953426a824d782a
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-05-08 20:44:37 -0700

    cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import Navbar from "~/components/Navbar";
-
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
