f6527bfee817d8b21671fce6a79ee86f61ced75b
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-05-08 20:49:50 -0700

    protect dash

--- src/pages/dash.tsx
+++ src/pages/dash.tsx
@@ -1,7 +1,28 @@
+import type { GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
 import React from "react";
+import { authOptions } from "../server/auth";
 
 const Dash = () => {
   return <div>dash</div>;
 };
 
 export default Dash;
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const session = await getServerSession(ctx.req, ctx.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: true,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+}
