5702b2cb1947645fcd5e4bcd9beab55810da6975
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-08-31 15:20:26 -0700

    add styling to surveys

--- src/pages/survey/[id].tsx
+++ src/pages/survey/[id].tsx
@@ -41,102 +41,112 @@ const SurveyPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
     return <h1 className="color mb-4 text-4xl ">Survey is no longer active</h1>;
 
   return (
-    <Formik
-      initialValues={{
-        answers: question.map((q) => {
-          if (q.type === "input" || q.type === "textArea") {
+    <div className=" flex h-screen flex-col items-center pt-9">
+      <Formik
+        initialValues={{
+          answers: question.map((q) => {
+            if (q.type === "input" || q.type === "textArea") {
+              return {
+                questionId: q.id,
+                text: "", // For text input questions
+              };
+            }
             return {
               questionId: q.id,
-              text: "", // For text input questions
+              value: 0, // For text input questions
             };
-          }
-          return {
-            questionId: q.id,
-            value: 0, // For text input questions
-          };
-        }),
-      }}
-      onSubmit={(values) => {
-        mutate({ answer: values.answers, surveyId: id });
-      }}
-    >
-      {({ values, handleSubmit, setFieldValue }) => {
-        console.log(values);
-        return (
-          <>
-            <h1 className="color mb-4 text-4xl ">{title}</h1>
-            <form onSubmit={handleSubmit}>
-              {question.map((q, index) => {
-                const { type, description } = question[index] || {};
-                return (
-                  <Fragment key={index}>
-                    {type === "number" && (
-                      <div>
-                        <label htmlFor={`answers.${index}.value`}>
-                          {description}
-                        </label>
-                        <input
-                          key={index}
-                          type="text"
-                          name={`answers.${index}.value`}
-                          value={values.answers[index]?.value}
-                          onChange={(e) => {
-                            if (isNaN(+e.target.value)) return;
-                            void setFieldValue(
-                              `answers.${index}.value`,
-                              +e.target.value
-                            );
-                          }}
-                        />
-                      </div>
-                    )}
-                    {type === "input" && (
-                      <div>
-                        <label htmlFor={`answers.${index}.text`}>
-                          {description}
-                        </label>
-
-                        <input
-                          key={index}
-                          type="text"
-                          name={`answers.${index}.text`}
-                          value={values.answers[index]?.text}
-                          onChange={(e) => {
-                            void setFieldValue(
-                              `answers.${index}.text`,
-                              e.target.value
-                            );
-                          }}
-                        />
-                      </div>
-                    )}
-                    {type === "textArea" && (
-                      <div className="flex w-[250px] flex-col">
-                        <label htmlFor={`answers.${index}.text`}>
-                          {description}
-                        </label>
-                        <textarea
-                          key={index}
-                          name={`answers.${index}.text`}
-                          value={values.answers[index]?.text}
-                          onChange={(e) => {
-                            void setFieldValue(
-                              `answers.${index}.text`,
-                              e.target.value
-                            );
-                          }}
-                        />
-                      </div>
-                    )}
-                  </Fragment>
-                );
-              })}
-              <button type="submit">Submit</button>
-            </form>
-          </>
-        );
-      }}
-    </Formik>
+          }),
+        }}
+        onSubmit={(values) => {
+          mutate({ answer: values.answers, surveyId: id });
+        }}
+      >
+        {({ values, handleSubmit, setFieldValue }) => {
+          console.log(values);
+          return (
+            <div className="flex flex-col gap-8">
+              <h1 className="color text-4xl ">{title}</h1>
+              <form onSubmit={handleSubmit}>
+                {question.map((q, index) => {
+                  const { type, description } = question[index] || {};
+                  return (
+                    <div key={index} className="flex gap-2">
+                      <div>{index + 1} -</div>
+                      {type === "number" && (
+                        <div className="mb-3 flex flex-col gap-2">
+                          <label htmlFor={`answers.${index}.value`}>
+                            {description}
+                          </label>
+                          <input
+                            key={index}
+                            type="text"
+                            name={`answers.${index}.value`}
+                            value={values.answers[index]?.value}
+                            className="input-bordered input w-full max-w-xs"
+                            onChange={(e) => {
+                              if (isNaN(+e.target.value)) return;
+                              void setFieldValue(
+                                `answers.${index}.value`,
+                                +e.target.value
+                              );
+                            }}
+                          />
+                        </div>
+                      )}
+                      {type === "input" && (
+                        <div className="mb-3 flex flex-col gap-2">
+                          <label htmlFor={`answers.${index}.text`}>
+                            {description}
+                          </label>
+                          <input
+                            key={index}
+                            type="text"
+                            name={`answers.${index}.text`}
+                            value={values.answers[index]?.text}
+                            className="input-bordered input w-full max-w-xs"
+                            onChange={(e) => {
+                              void setFieldValue(
+                                `answers.${index}.text`,
+                                e.target.value
+                              );
+                            }}
+                          />
+                        </div>
+                      )}
+                      {type === "textArea" && (
+                        <div className="mb-3 flex flex-col gap-2">
+                          <label htmlFor={`answers.${index}.text`}>
+                            {description}
+                          </label>
+                          <textarea
+                            key={index}
+                            name={`answers.${index}.text`}
+                            value={values.answers[index]?.text}
+                            className="textarea-bordered textarea"
+                            onChange={(e) => {
+                              void setFieldValue(
+                                `answers.${index}.text`,
+                                e.target.value
+                              );
+                            }}
+                          />
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+                <button
+                  type="submit"
+                  className="btn-secondary btn mt-9 max-w-[150px]"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          );
+        }}
+      </Formik>
+    </div>
   );
 };
 
